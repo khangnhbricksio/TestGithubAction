e49dd6109246bb9ecd40d18f6351f52192c83995
khangnh <khangnh.bricksio@gmail.com>
2024-01-13 17:52:54 +0700

    log files changes

--- build.js
+++ build.js
@@ -5,7 +5,9 @@ const argv = require("yargs").argv;
 async function main() {
   const files = argv._[0];
   
-  console.log("files", files);
+  console.log("files argv._[0]", files);
+  
+  console.log("files env", process.env.ALL_CHANGED_FILES);
 }
 
 main();
